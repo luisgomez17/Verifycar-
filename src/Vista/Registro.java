d22be929ba8b95f6fa659559d98760b08b86a5a8
JRamirezRGomez <ramirez.gomez1995@gmail.com>
2018-10-25 02:06:07 -0500

    Otra vez

--- src/Vista/Registro.java
+++ src/Vista/Registro.java
@@ -1156,25 +1156,43 @@ public class Registro extends javax.swing.JInternalFrame {
 
         rSPanelsSlider2.add(registroservicio, "card3");
 
+        registrofinalizado.setBackground(new java.awt.Color(255, 255, 255));
         registrofinalizado.setName("registrofinalizado"); // NOI18N
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 51, 55));
         jLabel2.setText("Registro Realizado");
 
+        jPanel13.setBackground(new java.awt.Color(255, 255, 255));
+
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel3.setText("Numero de Orden:");
+        jLabel3.setForeground(new java.awt.Color(51, 51, 55));
+        jLabel3.setText("Numero de Orden");
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(51, 51, 55));
         jLabel4.setText("Enviar por Correo");
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(51, 51, 55));
         jLabel5.setText("Enviar por Telefono");
 
         EnviarCorreo.setBackground(new java.awt.Color(237, 31, 36));
         EnviarCorreo.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         EnviarCorreo.setForeground(new java.awt.Color(255, 255, 255));
+        EnviarCorreo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Icon_SendEmail_32.png"))); // NOI18N
         EnviarCorreo.setText("Enviar");
         EnviarCorreo.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0))));
+        EnviarCorreo.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                EnviarCorreoMouseMoved(evt);
+            }
+        });
+        EnviarCorreo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                EnviarCorreoMouseExited(evt);
+            }
+        });
         EnviarCorreo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 EnviarCorreoActionPerformed(evt);
@@ -1184,8 +1202,19 @@ public class Registro extends javax.swing.JInternalFrame {
         jButton7.setBackground(new java.awt.Color(237, 31, 36));
         jButton7.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jButton7.setForeground(new java.awt.Color(255, 255, 255));
+        jButton7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Icon_SendPhoneNumber_32.png"))); // NOI18N
         jButton7.setText("Enviar");
         jButton7.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0))));
+        jButton7.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                jButton7MouseMoved(evt);
+            }
+        });
+        jButton7.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jButton7MouseExited(evt);
+            }
+        });
 
         lblGuia.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         lblGuia.setToolTipText("");
@@ -1195,36 +1224,37 @@ public class Registro extends javax.swing.JInternalFrame {
         jPanel13Layout.setHorizontalGroup(
             jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel13Layout.createSequentialGroup()
-                .addGap(29, 29, 29)
-                .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel3))
-                .addGap(47, 47, 47)
                 .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(EnviarCorreo, javax.swing.GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE)
-                    .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblGuia, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+                .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel13Layout.createSequentialGroup()
+                        .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(EnviarCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(54, 54, 54))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel13Layout.createSequentialGroup()
+                        .addComponent(lblGuia, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(20, 20, 20))))
         );
         jPanel13Layout.setVerticalGroup(
             jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel13Layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addGroup(jPanel13Layout.createSequentialGroup()
-                        .addGap(1, 1, 1)
-                        .addComponent(lblGuia, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(18, 18, 18)
+                .addGap(21, 21, 21)
+                .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lblGuia, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(17, 17, 17)
                 .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(EnviarCorreo))
-                .addGap(21, 21, 21)
-                .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel5)
-                    .addComponent(jButton7))
-                .addContainerGap(55, Short.MAX_VALUE))
+                    .addComponent(EnviarCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addContainerGap(67, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout registrofinalizadoLayout = new javax.swing.GroupLayout(registrofinalizado);
@@ -1245,7 +1275,7 @@ public class Registro extends javax.swing.JInternalFrame {
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel13, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(339, Short.MAX_VALUE))
+                .addContainerGap(295, Short.MAX_VALUE))
         );
 
         rSPanelsSlider2.add(registrofinalizado, "card4");
@@ -1865,6 +1895,30 @@ public class Registro extends javax.swing.JInternalFrame {
        btnEliminar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
        btnEliminar.setForeground(new java.awt.Color(255, 255, 255));
     }//GEN-LAST:event_btnEliminarMouseExited
+
+    private void EnviarCorreoMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_EnviarCorreoMouseMoved
+       EnviarCorreo.setBackground(new java.awt.Color(206, 26, 31));
+       EnviarCorreo.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+       EnviarCorreo.setForeground(new java.awt.Color(255, 255, 255)); 
+    }//GEN-LAST:event_EnviarCorreoMouseMoved
+
+    private void EnviarCorreoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_EnviarCorreoMouseExited
+       EnviarCorreo.setBackground(new java.awt.Color(237, 31, 36));
+       EnviarCorreo.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+       EnviarCorreo.setForeground(new java.awt.Color(255, 255, 255));
+    }//GEN-LAST:event_EnviarCorreoMouseExited
+
+    private void jButton7MouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton7MouseMoved
+       jButton7.setBackground(new java.awt.Color(206, 26, 31));
+       jButton7.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+       jButton7.setForeground(new java.awt.Color(255, 255, 255));
+    }//GEN-LAST:event_jButton7MouseMoved
+
+    private void jButton7MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton7MouseExited
+       jButton7.setBackground(new java.awt.Color(237, 31, 36));
+       jButton7.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+       jButton7.setForeground(new java.awt.Color(255, 255, 255));
+    }//GEN-LAST:event_jButton7MouseExited
     
     public void sendEmail(String Correo, String cliente) throws IOException {
         
